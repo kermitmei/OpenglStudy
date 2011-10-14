c0617c83b7e7fb2e93a507c4faf0e81d33e0054b
Kermit.Mei <kermit.mei@gmail.com>
2011-10-14 22:17:39 +0800

    gnustep is OK

--- objc/d1/main.m
+++ objc/d1/main.m
@@ -1,7 +1,20 @@
-#import <stdio.h>
+/******************* hello.m ***********************/
+#import <Foundation/Foundation.h>
+
+@interface HelloWorld : NSObject
+- (void) hello;
+@end
+
+@implementation HelloWorld
+- (void) hello {
+    NSLog(@"hello world!");
+}   
+@end
 
 int main(int argc, const char *argv[])
 {
+    NSMutableArray *foo;
+    foo = [NSMutableArray alloc];
     printf("Hello, world!\n");
     return 0;
 }
